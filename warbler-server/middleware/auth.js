5e139008ec223620641ab9c5354a23ff0ec2bf8a
Steve Johnson <steve21johnson@gmail.com>
2019-04-21 18:33:06 -0700

    messages

--- warbler-server/middleware/auth.js
+++ warbler-server/middleware/auth.js
@@ -1,3 +1,4 @@
+require("dotenv").config()
 const jwt = require("jsonwebtoken")
 
 exports.loginRequired = function (req, res, next) {
@@ -21,4 +22,20 @@ exports.loginRequired = function (req, res, next) {
   }
 }
 
-exports.ensureCorrectUser = function (req, res, next) { }
+exports.ensureCorrectUser = function (req, res, next) {
+  try {
+    const token = req.headers.authorization.split(" ")[1]
+    jwt.verify(token, process.env.SECRET_KEY, function (err, decoded) {
+      if (decoded && decoded.id === req.params.id) {
+        return next()
+      } else {
+        return next({
+          status: 401,
+          message: "Unauthorized"
+        })
+      }
+    })
+  } catch (e) {
+    return next({ status: 401, message: "Unauthorized" })
+  }
+}
--- warbler-server/models/message.js
+++ warbler-server/models/message.js
@@ -1,17 +1,21 @@
 const mongoose = require("mongoose")
 const User = require("./user")
 
-const messageSchema = new mongoose.Schema({
-  text: {
-    type: String,
-    required: true,
-    maxLength: 160
-  },
-  user: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User"
+const messageSchema = new mongoose.Schema(
+  {
+    text: {
+      type: String,
+      required: true,
+      maxLength: 160
+    },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User"
+    }
+  }, {
+    timestamps: true
   }
-})
+)
 
 messageSchema.pre("remove", async function (next) {
   try {
